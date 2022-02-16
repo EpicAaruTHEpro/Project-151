0b8a1a9c81d360c69698dec4a8ad461a1b2036d7
EpicAaruTHEpro <Atomicoolives@gmail.com>
2022-02-16 16:44:49 -0500

    Created Moving Car demo

---
+++ car.js
@@ -0,0 +1,23 @@
+AFRAME.registerComponent("movecar", {
+    schema : {
+        //rotationX: {type: "number", default: 1},
+        rotationY: {type: "number", default: 1},
+        //rotationZ: {type: "number", default: 1}
+    },
+    init: function (){
+        
+    },
+    update: function (){
+
+    },
+    tick: function (){
+        //this.data.rotationX += 0.05
+        this.data.rotationY += 0.3
+        //this.data.rotationZ += 0.03
+        var rotation = this.el.getAttribute("rotation")
+        //rotation.x = this.data.rotationX
+        rotation.y = this.data.rotationY
+        //rotation.z = this.data.rotationZ
+        this.el.setAttribute("rotation", {y: rotation.y,})
+    }
+})
